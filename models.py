08e8c18cc66d0060da223ef54b463911b46aae9e
Zulfikar Akbar Muzakki <zakki@kartoza.com>
2024-08-01 10:41:15 +0700

    Remoe User import

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 import os
 from django.utils import timezone
 from django.contrib.gis.db import models
-from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
 
